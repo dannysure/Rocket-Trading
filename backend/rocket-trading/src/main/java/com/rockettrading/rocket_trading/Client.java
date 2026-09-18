bb4c59ba7a36e0a14c9251a06d76ee1c9703ca40
Sam Adams <119465691+SamAdams1@users.noreply.github.com>
2026-09-18 15:50:08 -0400

    basic class and test implementation

---
+++ backend/rocket-trading/src/main/java/com/rockettrading/rocket_trading/Client.java
@@ -0,0 +1,8 @@
+package com.rockettrading.rocket_trading;
+
+public class Client {
+    private Long clientID;
+    private String clientName;
+    private String clientAddress;
+
+}
---
+++ backend/rocket-trading/src/test/java/com/rockettrading/rocket_trading/RocketTradingApplicationTests.java
@@ -0,0 +1,20 @@
+package com.rockettrading.rocket_trading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class RocketTradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	public void test() {
+		assertEquals(2+2, 4);
+	}
+
+}
